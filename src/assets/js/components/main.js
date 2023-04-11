31f411ea00cb9ca575ac1810a2f261f24cfb7819
Daniil <karndaniil@gmail.com>
2023-04-11 17:48:02 +0500

    7

--- src/assets/js/components/main.js
+++ src/assets/js/components/main.js
@@ -1,27 +1,27 @@
 
 //= components/form.js
 
-const modal = document.querySelector('.modal')
-const talkBtn = document.querySelector('.btn_talk_link')
+document.querySelector(".modal");
+const talkBtn = document.querySelector(".btn_talk_link");
 talkBtn.addEventListener("click",()=>{
-    let email = document.querySelector('.email')
-    email.value = ''
-    document.querySelector('.modal').classList.add('open')
-    document.body.classList.add('openBody')
-    document.body.scrollTo(120,0)
-})
+    let email = document.querySelector(".email");
+    email.value = "";
+    document.querySelector(".modal").classList.add("open");
+    document.body.classList.add("openBody");
+    document.body.scrollTo(120,0);
+});
 
-const close = document.querySelector('.close').addEventListener("click",(e)=>{
-    document.querySelector('.modal').classList.remove('open')
-    document.body.classList.remove("openBody")
+document.querySelector(".close").addEventListener("click",()=>{
+    document.querySelector(".modal").classList.remove("open");
+    document.body.classList.remove("openBody");
     
-})
+});
 
-document.querySelector('.modal').addEventListener("click", (e)=>{
-    document.querySelector('.modal').classList.remove('open')
-    document.body.classList.remove("openBody")
-})
+document.querySelector(".modal").addEventListener("click", ()=>{
+    document.querySelector(".modal").classList.remove("open");
+    document.body.classList.remove("openBody");
+});
 
-const modalBox = document.querySelector('.modal_box').addEventListener('click', (e) => {
-    e.stopPropagation()
-})
+document.querySelector(".modal_box").addEventListener("click", (e) => {
+    e.stopPropagation();
+});
